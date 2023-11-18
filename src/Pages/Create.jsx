a525be012c255fdf8a341d4bb905f6f432364d0d
belemaaa <graceitamunoala@gmail.com>
2023-11-18 16:56:08 +0000

    changed signup page

--- src/Pages/Create.jsx
+++ src/Pages/Create.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import './Create.css'
+import './Login.css'
 import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
@@ -41,31 +41,43 @@ const Create = () => {
       }
     }
   return (
-    <div className='Signup'>
-      <div className='form2Holder'>
-        <section className='section1'>
-        <label className="formheader">  <h1>Signup</h1> </label>
-          <form className='form2' onSubmit={handleSubmit}>
-          <i class="fi fi-tr-circle-user"></i>
-            <input placeholder='First name' type='text' value={first_name} onChange={(e) => setFirst_name(e.target.value)} >
-            </input><br></br>
+    <div className='Logins'>
+      <section className='section2'>
+        <div className='formHolder'>
+            <h1 className='welcome'>Signup</h1>
+            <form  className='formm' onSubmit={handleSubmit}> 
+              <svg className='svg1' xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 27 27" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg><br></br>
 
-            <input placeholder='Last name' type='text' value={last_name} onChange={(e) => setLast_name(e.target.value)}>
-            </input><br></br>
+                <label>First name</label><br></br>
+                <input placeholder='Input your first name' type='text' value={first_name} onChange={(e) => setFirst_name(e.target.value)} >
+                </input>
+                <br/><br/>
 
-            <input placeholder='Username' type='text' value={username} onChange={(e) => setUsername(e.target.value)}>
-            </input><br></br>
+                <label>Last name</label><br></br>
+                <input placeholder='Input your last name' type='text' value={last_name} onChange={(e) => setLast_name(e.target.value)} >
+                </input>
+                <br/><br/>
 
-            <input placeholder='Email' type='email' value={email} onChange={(e) => setEmail(e.target.value)}>
-            </input><br></br>
+                <label>Email</label><br></br>
+                <input placeholder='Input your Email' type='email' value={email} onChange={(e) => setEmail(e.target.value)} >
+                </input>
+                <br/><br/>
 
-            <input placeholder='password' type='password' value={password} onChange={(e) => setPassword(e.target.value)}>
-            </input><br></br>
+                <label>Phone number</label><br></br>
+                <input placeholder='Input your phone number' type='phone' value={phone_number} onChange={(e) => setPhone_number(e.target.value)} >
+                </input>
+                <br/><br/>
 
-            <button type='submit'>Sign up</button>
-          </form>
-        </section>
-      </div>
+                <label>Password</label><br></br>
+                <input placeholder= 'Input your password' type='password' value={password} onChange={(e) => setPassword(e.target.value)} >
+                </input>
+                <br/><br/>
+                <button type='submit'><p>Create account</p></button>
+                <p className='last1'>Not a new user?<Link to='/Login'> Login</Link> </p>
+            </form> 
+            
+        </div>
+      </section>
 
     </div>
   )
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -5,9 +5,6 @@ import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
-
-
-
 const Login = () => {
     
         const [email, setEmail] = useState ('')
@@ -18,26 +15,16 @@ const Login = () => {
             axios.post('http://localhost:8082/login', {email , password})
             .then(res => console.log(res))
             // .catch(err => console.log(error))
-        }
-
-
-    
+        } 
     const welcome = ' Login'
-
-
-
-
   return (
 
     <div className='Logins'>
          <section className='section2'>
         <div className='formHolder'>
             <h1 className='welcome'>{welcome}</h1>
-            
-
-            <form  className='formm' onSubmit={handleSubmit}>
-            
-            <svg className='svg1' xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 27 27" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg><br></br>
+            <form  className='formm' onSubmit={handleSubmit}> 
+              <svg className='svg1' xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 27 27" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg><br></br>
 
                 <label>Email</label><br></br>
                 <input placeholder='Input your Email' type='email' name='email' onChange={e => setEmail(e.target.value)} >
